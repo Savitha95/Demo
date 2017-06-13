c18f1492574924383ca1484060fd49c7566117e1
Savitha95 <savithaksrct@gmail.com>
2017-06-13 10:56:13 +0530

    Create guvi.java

---
+++ guvi.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+public class guvi
+{
+public static void main(String args[])
+{
+int n;
+Scanner sc=new Scanner(System.in);
+n=sc.nextInt();
+if(n==0)
+System.out.println("ZERO");
+else if(n<0)
+System.out.println("NEGATIVE");
+else
+System.out.println("POSITIVE");
+}
+}
