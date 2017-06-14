c152fda8f6ebf35c248646393654451028dd80cc
Savitha95 <savithaksrct@gmail.com>
2017-06-14 14:06:35 +0530

    Create oe.java

---
+++ oe.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+class oe
+{
+public static void main(String args[])
+{
+int n;
+Scanner sc=new Scanner(System.in);
+n=sc.nextInt();
+if(n%2==0)
+System.out.println(n+ "is EVEN");
+else
+System.out.println(n+ "is ODD");
+}
+}
