c9722a5d6516947735d5a6e673a6354a05ad344e
Savitha95 <savithaksrct@gmail.com>
2017-06-15 10:06:47 +0530

    Create string.java

---
+++ string.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+public class strint
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+StringBuffer sb=new StringBuffer("savitha");
+sb.reverse();
+System.out.println(sb);
+}
+}
