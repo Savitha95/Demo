10c1f678d06a6a66a1409c8a14c89c816fd55648
Savitha95 <savithaksrct@gmail.com>
2017-06-16 10:07:45 +0530

    Update and rename hello.java to hellohello.java

---
+++ hellohello.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+public class hellohello
+{
+public static void main(String args[])
+{
+int n;
+Scanner sc=new Scanner(System.in);
+n=sc.nextInt();
+for(i=0;i<=n;i++)
+{
+System.out.println("hello");
+}
+}
+}
