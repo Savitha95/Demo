594a43f7080cc7b642b9829cc0598e3ecbefabeb
Savitha95 <savithaksrct@gmail.com>
2017-06-16 12:33:58 +0530

    Create large.java

---
+++ large.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+public class large
+{
+public static void main(String args[])
+{
+int n,i;
+int a[]=new int[3];
+int large=a[0];
+Scanner sc=new Scanner(System.in)
+n=sc.nextInt();
+for(i=1;i<a.length;i++)
+{
+a[i]=sc.nextInt();
+if(a[i]>large)
+large=a[i];
+}
+System.out.println("large");
+}
+}
