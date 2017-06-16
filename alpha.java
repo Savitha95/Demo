f56688f75bd9c5294610eaae93a4357b1ad62ecc
Savitha95 <savithaksrct@gmail.com>
2017-06-16 09:57:34 +0530

    Update and rename alphabet.java to alpha.java

---
+++ alpha.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+public class alpha
+{
+public static void main(String args[])
+{
+char ch;
+Scanner sc=new Scanner(System.in);
+ch=sc.next().charAt(0);
+if((ch>='a' && ch<='z')||(ch>='A' && ch<='Z'))
+System.out.println("alphabet");
+else
+System.out.println("not a alphabet");
+}
+}
