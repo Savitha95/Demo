50d0837076a96c732d730cc6706ae2a7dd6991e5
Savitha95 <savithaksrct@gmail.com>
2017-06-19 14:18:56 +0530

    Update large.java

--- large.java
+++ large.java
@@ -7,7 +7,7 @@ public static void main(String args[])
 int n,i;
 int a[]=new int[3];
 int large=a[0];
-Scanner sc=new Scanner(System.in)
+Scanner sc=new Scanner(System.in);
 n=sc.nextInt();
 for(i=1;i<a.length;i++)
 {
@@ -15,6 +15,6 @@ a[i]=sc.nextInt();
 if(a[i]>large)
 large=a[i];
 }
-System.out.println("large");
+System.out.println(large);
 }
 }
