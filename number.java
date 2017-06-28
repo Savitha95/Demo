310d3e8bc7b1b245bab3be7aabb282b62bfbf622
Savitha95 <savithaksrct@gmail.com>
2017-06-28 14:27:47 +0530

    Create number.java

---
+++ number.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+class number
+{
+public static void main(String args[])
+{
+int n,sum=0;
+Scanner sc=new Scanner(System.in);
+n=sc.nextInt();
+for(int i=0;i<=n;i++)
+{
+sum=(sum+i);
+}
+System.out.println(sum);
+}
+}
+
