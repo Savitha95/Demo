5ae66c355158b50ba281f90621c54a49517cd2f9
Savitha95 <savithaksrct@gmail.com>
2017-06-28 13:48:30 +0530

    Update convert.java

--- convert.java
+++ convert.java
@@ -6,7 +6,7 @@ public static void main(String args[])
 {
 Scanner sc=new Scanner(System.in);
 String s=sc.next();
-int n=Integer.parseInt();
+int n=Integer.parseInt(s);
 System.out.println(n);
 }
 }
