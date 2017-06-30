6ee915054fbdf2f4df83c6156568db39e93c83de
Savitha95 <savithaksrct@gmail.com>
2017-06-30 10:27:44 +0530

    Create amstrong.java

---
+++ amstrong.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+class amstrong
+{
+public static void main(String args[])
+{
+int c=0,a,temp;
+int n=153;
+temp=n;
+while(n>0)
+{
+a=n%10;
+n=n/10;
+c=c+(a*a*a);
+}
+if(temp==c)
+System.out.println("amstrong number");
+}
+}
