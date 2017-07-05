72f3d14b7f26800053017f894e74c57662565268
Savitha95 <savithaksrct@gmail.com>
2017-07-05 15:41:33 +0530

    Create multab.java

---
+++ multab.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+public class multab
+{
+public static void main(String args[])
+{
+int n,i,k;
+Scanner sc=new Scanner(System.in);
+n=sc.nextInt();
+for(i=1;i<=10;i++)
+{
+k=n*i;
+System.out.println(n+"*"+i+"="+k);
+}
+}
+}
+
