ec54ea98a2dc0e6f8bdefe1af4c92111ad00257a
Savitha95 <savithaksrct@gmail.com>
2017-07-11 10:33:43 +0530

    Create betweenprime.java

---
+++ betweenprime.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+public class betweenprime
+{
+public static void main(String args[])
+{
+int s1,s2,s3,flag=0,i,j;
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter the lower limit:");
+int s1=sc.next();
+System.out.println("Enter the upper limit:");
+int s2=sc.next();
+System.out.println("The prime numbers 
