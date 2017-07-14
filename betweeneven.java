740959fe56635e8e16585349fdfbba33a335ad6d
Savitha95 <savithaksrct@gmail.com>
2017-07-14 09:47:28 +0530

    Update betweeneven.java

--- betweeneven.java
+++ betweeneven.java
@@ -7,4 +7,10 @@ public static void main(String args[])
 Scanner sc=new Scanner(System.in);
 int a=sc.nextInt();
 int b=sc.nextInt();
-for(i=a;i<=
+for(i=a;i<=b;i++)
+{
+  if(i%2==0)
+    System.out.println(i);
+}
+}
+}
